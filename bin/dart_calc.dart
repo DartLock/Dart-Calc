0e3ad328d634678605c8d8d1cef8264d9ab0e59a
Vitaly Simakov <v.simakov@tdm-tech.ru>
2024-08-19 14:03:36 +0500

    fix: удалил отладку

--- bin/dart_calc.dart
+++ bin/dart_calc.dart
@@ -1,20 +1,26 @@
 import 'package:dart_calc/combiner.dart';
+import 'dart:io';
 
 void main() {
-  String sourceExpression = "- 5 + (3 + 2 - 3 * 6 + 2) / 3 + (1 * 2 + (6 + 8) * 4)";
-  print("source expression : ${sourceExpression}");
+  String? defaultExpression = "-5 + (3 + 2 - 3 * 6 + 2) / 3 + (1 * 2 + (6 + 8) * 4)";
 
+  print("Правила ввода выражения:");
+  print("1. Между операторами и операндом должны быть пробелмы. Пример: 3 + 4");
+  print("2. Перед скобками снаружи должны быть пробелмы. Пример: 2 + (3 + 4) - 1");
+  print("3. Отрицательные числа указываются слитно со знаком минус. Пример: -1");
+  print("Пример правильно написанного выражения : ${defaultExpression}");
 
-  //while(true) {
-  // print("Enter math expression OR 'q' for exit");
-  //
-  // final input = stdin.readLineSync();
-  //
-  // if (input == 'q') break;
+  while(true) {
+    print("Enter math expression OR 'q' for exit");
 
-  // sourceExpression = sourceExpression!.replaceAll(new RegExp(r'\s'), '');
-  Combiner.calculate(sourceExpression);
-  //}
+    final input = stdin.readLineSync();
 
-  print(Combiner.result);
+    if (input == 'q') break;
+
+    if (input.toString().length > 0) defaultExpression = input;
+
+    Combiner.calculate(defaultExpression!);
+
+    print("expression result: ${Combiner.result}");
+  }
 }
--- lib/combiner.dart
+++ lib/combiner.dart
@@ -1,12 +1,9 @@
 import 'dart:math';
-import 'dart:io';
 
 typedef OperationProcessing = String Function();
 
 class Combiner {
   static late String result;
-  static int match_count = 1;
-
   static final roundBrakesPattern = new RegExp(r"\(-\d+\)|\((-?\d+\.\d+?)\)", caseSensitive: false);
   static final numericPattern = new RegExp(r"(-?\d+\.\d+?)|(-?\d+?)");
   static final operatorPattern = new RegExp(r"\s[\-\+\/\*]\s");
@@ -19,45 +16,14 @@ class Combiner {
   static final dividePattern = new RegExp(r"(-?\d+(\.\d+)?) \/ (-?\d+(\.\d+)?)", caseSensitive: false);
   static final multiplePattern = new RegExp(r"(-?\d+(\.\d+)?) \* (-?\d+(\.\d+)?)", caseSensitive: false);
 
-  static String negativeFormat(String expression) {
-    List splittedExpression = expression.split(r"");
-
-    return expression;
-  }
-
-  static String _negativeFormat(Match match) {
-    print("_negativeFormat match[0]: ${match[0]}");
-
-    Iterable<Match> numbersMatches = numericPattern.allMatches(match[0]!, 0);
-    final first = numbersMatches.elementAt(0)[0];
-    final second = numbersMatches.elementAt(1)[0];
-
-    print("first: ${first}");
-    print("second: ${second}");
-
-    final operator = operatorPattern.allMatches(match[0]!, 0).elementAt(0)[0]!.trim();
-
-    return Combiner.compute(first, second, operator).toString();
-  }
-
-  static String? calculate(String sourceExpression) {
+  static void calculate(String sourceExpression) {
     while(true) {
-      print("");
-      print("Enter 'n' for next or 'q' for exit");
-      print("Current Expression: $sourceExpression");
-
-      final input = stdin.readLineSync();
-
-      if (input.toString().toLowerCase() == 'q') break;
-
       sourceExpression = operationProcessing(multiplePattern, sourceExpression);
       sourceExpression = operationProcessing(dividePattern, sourceExpression);
       sourceExpression = operationProcessing(divideSinglePattern, sourceExpression);
       sourceExpression = operationProcessing(multipleSinglePattern, sourceExpression);
       sourceExpression = operationProcessing(subPattern, sourceExpression);
       sourceExpression = operationProcessing(addPattern, sourceExpression);
-
-      sourceExpression = negativeFormat(sourceExpression);
 
       try {
         result = (double.parse(sourceExpression)).toString();
@@ -70,29 +36,15 @@ class Combiner {
   }
 
   static String operationProcessing(RegExp pattern, String expression) {
-    print("");
-    print("operationProcessing pattern: ${pattern.toString()}");
-
-    match_count = 1;
     expression = expression.replaceAllMapped(pattern, Combiner.computeProcessing);
-
-    print("operationProcessing ${pattern.toString()} END");
-    print("expression: ${expression}");
 
     return removeRoundBrakes(expression);
   }
 
   static String computeProcessing(Match match) {
-    print("${match_count}. computeProcessing match[0]: ${match[0]}");
-    match_count++;
-
     Iterable<Match> numbersMatches = numericPattern.allMatches(match[0]!, 0);
     final first = numbersMatches.elementAt(0)[0];
     final second = numbersMatches.elementAt(1)[0];
-
-    print("first: ${first}");
-    print("second: ${second}");
-
     final operator = operatorPattern.allMatches(match[0]!, 0).elementAt(0)[0]!.trim();
 
     return Combiner.compute(first, second, operator).toString();
@@ -116,22 +68,15 @@ class Combiner {
   }
 
   static String removeRoundBrakes(String expression) {
-    print("removeRoundBrakes 1: $expression");
     expression = expression.replaceAllMapped(roundBrakesPattern, _removeRoundBrakes);
-    print("removeRoundBrakes 2: $expression");
 
     return expression;
   }
 
   static String _removeRoundBrakes(Match match) {
-    print("_removeRoundBrakes match[0]: ${match[0]}");
-
     var number = match[0]!;
-
     number = number.replaceAll(r"(", "");
     number = number.replaceAll(r")", "");
-
-    print("_removeRoundBrakes replaced: $number");
 
     return number;
   }
